e2c4d32f32792a10c26c8e1b8c735d7d61f11567
PoseidonOcto <octopus0246@gmail.com>
2024-01-08 18:39:26 +1000

    Added autopairs and escaping with 'jk'

--- init.lua
+++ init.lua
@@ -326,6 +326,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Alternate method of escaping to normal mode
+vim.keymap.set('i', 'jk', '<esc>')
+vim.keymap.set('i', 'kj', '<esc>')
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
